99189f5bcaca4c265e8cc14d3a413deff7e681a6
creppert308 <creppert@ycp.edu>
2025-11-07 23:05:07 -0500

    Started Adding another data file

--- ycpHacks/DemoPlot.py
+++ ycpHacks/DemoPlot.py
@@ -2,11 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('104345.csv')
+da = pd.read_csv('Mobilgrease 28.csv')
 
 df = pd.read_csv('104345.csv', skiprows=1)
+da = pd.read_csv('Mobilgrease 28.csv', skiprows=1)
+
 print(df.head())
 
 plt.plot(df['cm-1'], df['A'])
 
-plt.show()
+plt.xlabel('cm-1')
+plt.ylabel('A')
 
+plt.show()
