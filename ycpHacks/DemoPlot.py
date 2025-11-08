842cb1ed3d35be5db95bd0dab6385453e4802b71
creppert308 <creppert@ycp.edu>
2025-11-07 22:31:50 -0500

    Update DemoPlot.py

--- ycpHacks/DemoPlot.py
+++ ycpHacks/DemoPlot.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-sample_base = pd.read_csv("C:\YCPHacks25\YCPHacks2025_MRGLabs\ycpHacks\104345.csv")
+df = pd.read_csv('104345.csv')
 
-plt.title("Test Plot")
-plt.plot(sample_base.cm-1, sample_base.A, linewidth = 1)
+df = pd.read_csv('104345.csv', skiprows=1)
+print(df.head())
+
+plt.plot(df['cm-1'], df['A'])
 
 plt.show()
 
