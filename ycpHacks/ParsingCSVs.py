3b50c4f5d2354d03d0448dc96b7a1684d4ee58f0
Caroline Young <Cyoung29@ycp.edu>
2025-11-07 21:29:40 -0500

    Parsing CSVs, first try, works for now, using pandas

--- ycpHacks/ParsingCSVs.py
+++ ycpHacks/ParsingCSVs.py
@@ -1,7 +1,22 @@
-import csv
+import pandas as pd
 
-with open('mobilegrease 28.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
+df = pd.read_csv('/Users/caroline/PycharmProjects/YCPHacks2025_MRGLabs/ycpHacks/Mobilgrease 28.csv')
 
-    for row in reader:
-        print(row)
+#show first 5 rows
+print(df.head())
+
+#show column names
+print(df.columns)
+
+#show number of rows and columns
+print(df.shape)
+
+#Access one column
+#print(df['A'])
+
+#filter data
+#filtered = df[df['A'] < 4000]
+#print(filtered)
+
+# Access a specfic row
+print(df.iloc[0])#first row
