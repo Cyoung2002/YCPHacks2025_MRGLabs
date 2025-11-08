a827e90eea3b291046bc6395583e34d142104650
creppert308 <creppert@ycp.edu>
2025-11-07 23:13:23 -0500

    Got two plots working YAY!

--- ycpHacks/DemoPlot.py
+++ ycpHacks/DemoPlot.py
@@ -9,9 +9,12 @@ da = pd.read_csv('Mobilgrease 28.csv', skiprows=1)
 
 print(df.head())
 
-plt.plot(df['cm-1'], df['A'])
+plt.plot(df['cm-1'], df['A'], label='104345', linewidth=1)
+plt.plot(da['cm-1'], da['A'], label='Mobilgrease 28', linewidth=1)
 
 plt.xlabel('cm-1')
 plt.ylabel('A')
+
+plt.legend()
 
 plt.show()
